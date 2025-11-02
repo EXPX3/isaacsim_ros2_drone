d66a34cca8849c3bb38942bd8caf572f219af77b
giridhar vb <robotgir6937@gmail.com>
2025-11-02 14:10:00 +0100

    4_5related

--- docker/launch_docker.sh
+++ docker/launch_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xhost +local:
 
-docker run --name isaac-sim-ros2 --entrypoint bash -it --gpus device=1 -e "ACCEPT_EULA=Y" --rm --network=host --privileged \
+docker run --name isaac-sim-ros2 --entrypoint bash -it --gpus all -e "ACCEPT_EULA=Y" --rm --network=host --privileged \
     -v ~/docker/isaac-sim-ros2/cache/kit:/isaac-sim/kit/cache/Kit:rw \
     -v ~/docker/isaac-sim-ros2/cache/ov:/root/.cache/ov:rw \
     -v ~/docker/isaac-sim-ros2/cache/pip:/root/.cache/pip:rw \
@@ -18,4 +18,4 @@ docker run --name isaac-sim-ros2 --entrypoint bash -it --gpus device=1 -e "ACCEP
     --workdir="/root/colcon_ws" \
     --volume="$(pwd)/../colcon_ws:/root/colcon_ws" \
     --volume="$(pwd)/../lidar_cfg/hokuyo:/isaac-sim/exts/omni.isaac.sensor/data/lidar_configs/hokuyo" \
-    isaac-ros2-image:latest	
+    isaac-ros2-image_4-5:latest	
